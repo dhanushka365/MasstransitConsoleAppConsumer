9141bc401b0c1e199043dc19fa94f288270efff1
pasindu uduwela <puduwela@icptechno.com>
2024-01-15 20:48:08 +0530

    Add project files.

---
+++ MasstransitConsoleAppConsumer/EventConsumer.cs
@@ -0,0 +1,14 @@
+﻿using EventContract;
+using MassTransit;
+
+namespace MasstransitConsoleAppConsumer
+{
+    public class EventConsumer : IConsumer<ValueEntered>
+    {
+        public async  Task Consume(ConsumeContext<ValueEntered> context)
+        {
+            Console.WriteLine("ValueEntered: {0}", context.Message.Value);
+        }
+       
+    }
+}
---
+++ MasstransitConsoleAppConsumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using EventContract;
+using MassTransit;
+using MasstransitConsoleAppConsumer;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+        {
+        
+            cfg.ReceiveEndpoint("EventContract:"+nameof(ValueEntered), e =>
+            {
+                e.Consumer<EventConsumer>();
+            });
+        });
+    
+        var source = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+
+        await busControl.StartAsync(source.Token);
+
+        try
+        {
+            Console.WriteLine("Press any key to exit");
+            await Task.Run(Console.ReadKey);
+        }
+        finally
+        {
+            await busControl.StopAsync();
+        }
+    }
+}
